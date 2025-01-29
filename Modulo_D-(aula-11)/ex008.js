56ff145c8cfaa24796478f6ac9a711f7fd48672d
Mariabr200 <mariaem@baraodemaua.br>
2025-01-29 15:03:26 -0300

    adicionei a aula 11, ex009

---
+++ Modulo_D-(aula-11)/ex008.js
@@ -0,0 +1,33 @@
+/*
+  Sequências:
+  var n = 3
+  n += 2
+  window.alert(n)
+  Condições:
+  if (condição) {
+    true
+  } else {
+    false
+  }
+  Tipos de condição:
+    -> condição simples:
+      if (condiçao) {
+      true
+      }
+    -> condição composta:
+      if (condiçao) {
+      true
+      } else {
+       false
+      }
+*/
+//console.log("O console funcionou corretamente!");
+
+var vel = 55;
+console.log(`A velocidade do seu carro é ${vel}Km/h`);
+if (vel > 60) {
+  //ex: condição simples
+  console.log(`Você ultrapassou a velocidade permitida. MULTADO!`);
+}
+
+console.log(`Dirija sempre usando o cinto de segurança`);
---
+++ Modulo_D-(aula-11)/ex009.js
@@ -0,0 +1,8 @@
+var país = "Ucrânia";
+console.log(`Vivendo em ${país}`);
+if (país == "Brasil") {
+  //ex de condição composta
+  console.log("Você é Brasileiro");
+} else {
+  console.log("Você é um Estrangeiro");
+}
