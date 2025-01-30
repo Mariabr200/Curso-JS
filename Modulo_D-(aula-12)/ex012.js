8fd1447cd05b166c098dd05f1d74fb9327962d2f
Mariabr200 <mariaem@baraodemaua.br>
2025-01-30 10:35:26 -0300

    aula 12, ex012

---
+++ Modulo_D-(aula-12)/ex012.js
@@ -0,0 +1,9 @@
+var hora = 4;
+console.log(`Agora são exatamente ${hora} horas.`);
+if (hora > 6 && hora < 12) {
+  console.log("Bom dia!");
+} else if (hora >= 12 && hora <= 18) {
+  console.log("Boa Tarde!");
+} else if (hora > 0 && hora < 5) {
+  console.log("Boa Madrugada!");
+}
