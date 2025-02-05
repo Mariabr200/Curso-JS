6f1b9fd09d372e9692d1510e7578d6ee46f6410c
Mariabr200 <mariaem@baraodemaua.br>
2025-02-05 15:02:57 -0300

    criando a aula 15, do módulo F

---
+++ Modulo_F-(aula-15)/ambiente.js
@@ -0,0 +1,5 @@
+let num = [5, 6, 7, 9];
+num.sort() //coloca em ordem
+num.push(1) //adiciona um número no final 
+console.log(`O vetor tem ${num} posições`);
+console.log(`O primeiro valor do vetor é ${num[0]}`);
