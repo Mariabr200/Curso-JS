1972ebcb46c5483eb1f896ad8232fe42c009c79c
Mariabr200 <mariaem@baraodemaua.br>
2025-02-06 08:44:11 -0300

    modulo F, aula 15
    fiz algumas mudanças no exercício (ambiente) e adicionei o exercício (vetor)

--- Modulo_F-(aula-15)/ambiente.js
+++ Modulo_F-(aula-15)/ambiente.js
@@ -1,5 +1,6 @@
 let num = [5, 6, 7, 9];
-num.sort() //coloca em ordem
-num.push(1) //adiciona um número no final 
+num.sort(); //coloca em ordem
+num.push(1); //adiciona um número no final
+console.log(num);
 console.log(`O vetor tem ${num} posições`);
 console.log(`O primeiro valor do vetor é ${num[0]}`);
